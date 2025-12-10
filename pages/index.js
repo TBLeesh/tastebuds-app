25a350598c42c59025dcd62d63e52a4993e75cd8
TBLeesh <elisha.fischer@outlook.com>
2025-12-10 13:39:15 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      fontFamily: "Arial", 
+      padding: "40px",
+      textAlign: "center"
+    }}>
+      <h1>Welcome to TasteBuds</h1>
+      <p>Find foodie friends. Explore new flavors. Share delicious moments.</p>
+    </div>
+  );
+}
